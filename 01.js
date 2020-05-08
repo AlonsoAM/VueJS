8e0b4ff5c872bbdbeff908e9f0e0d1c83ee34f64
AlonsoAM <alonso.amoreno@live.com>
2020-05-08 08:59:56 -0500

    Agregando

--- 01.js
+++ 01.js
@@ -5,25 +5,37 @@ const app = new Vue({
         estudiantes:[
             {
                 nombre: "Damarys",
-                nota: "12"
+                nota: 12
             },
             {
                 nombre: "donna",
-                nota: "14"
+                nota: 14
             },
             {
                 nombre: "Diego",
-                nota: "10"
+                nota: 10
             }
         ],
         estudiante: '',
-        nota: ''
+        nota: '',
+        total : 0
     },
     methods: { // Los metodos que me permiten interactuar con HTML
         agregaEstudiante (){
             this.estudiantes.push({
                 nombre: this.estudiante, nota: this.nota
             })
+            this.estudiante = ""
+            this.nota = ""
+        }
+    },
+    computed: {
+        sumarNotas(){
+            this.total = 0
+            for(n of this.estudiantes){
+                this.total = this.total + n.nota
+            }
+            return this.total
         }
     }
 })
